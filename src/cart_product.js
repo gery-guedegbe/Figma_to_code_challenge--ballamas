9d58a8f43d626f26a61bcb2a5bbd8aaeeb5655ac
gery-guedegbe <guedegbe2003@gmail.com>
2024-08-13 23:27:18 +0100

    Update files

--- src/cart_product.js
+++ src/cart_product.js
@@ -1,6 +1,6 @@
 const cartProduct = [
   {
-    image: "../assets/images/person_5.png",
+    image: "../src/assets/images/person_5.png",
     size: "Green Large",
     price: 100,
   },
@@ -10,17 +10,17 @@ const cartProduct = [
     price: 100,
   },
   {
-    image: "../assets/images/person_5.png",
+    image: "../src/assets/images/person_5.png",
     size: "Green Large",
     price: 100,
   },
   {
-    image: "../assets/images/person_6.png",
+    image: "../src/assets/images/person_6.png",
     size: "Green Large",
     price: 100,
   },
   {
-    image: "../assets/images/jordan_shoes.png",
+    image: "../src/assets/images/jordan_shoes.png",
     size: "Green Large",
     price: 100,
   },
